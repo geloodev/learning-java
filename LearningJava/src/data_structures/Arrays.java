a3fcca5f9e79e52152651c3fe16542d985b2db6f
geloodev <geloodev@pop-os.localdomain>
2023-04-28 10:32:56 -0300

    :sparkles: feat: arrays and multidim arrays (data structures)

---
+++ LearningJava/src/data_structures/Arrays.java
@@ -0,0 +1,28 @@
+package data_structures;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        String[] names = new String[3];
+        names[0] = "Luffy";
+        names[1] = "Goku";
+        names[2] = "Hinata";
+        //for loop with arrays
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+        
+        int[] ages = {1, 2, 3, 4, 5}; //same as int ages = new int[]{1, 2, 3, 4, 5};
+        //foreach loop with arrays
+        for (int age: ages) {
+            System.out.println(age);
+        }
+
+        /* DEFAULT INITIALIZATION VALUES:
+        byte, short, double, float, long, int = 0
+        char = '\u0000' (empty)
+        boolean = false
+        String = null
+         */
+    }
+}
---
+++ LearningJava/src/data_structures/MultidimensionalArrays.java
@@ -0,0 +1,28 @@
+package data_structures;
+
+public class MultidimensionalArrays {
+
+    public static void main(String[] args) {
+        int[][] days = new int[2][2];
+        days[0][0] = 31;
+        days[0][1] = 28;
+        days[1][0] = 31;
+        days[1][1] = 30;
+
+        //using for
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                System.out.println(days[i][j]);
+            }
+        }
+
+        System.out.println("------------------");
+
+        //using foreach
+        for (int[] baseArray: days) {
+            for (int day: baseArray) {
+                System.out.println(day);
+            }
+        }
+    }
+}
