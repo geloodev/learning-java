80f5824d8d32c5c078ddf052c036ddeb1b9476d0
geloodev <geloodev@pop-os.localdomain>
2023-05-10 09:57:34 -0300

    :fire: deleting introduction_classes folder

--- LearningJava/src/b_object_orientation/a_introduction_classes/Professor.java
+++
@@ -1,7 +0,0 @@
-package b_object_orientation.a_introduction_classes;
-
-public class Professor {
-    public String name;
-    public int age;
-    public char gender;
-}
--- LearningJava/src/b_object_orientation/a_introduction_classes/Student.java
+++
@@ -1,7 +0,0 @@
-package b_object_orientation.a_introduction_classes;
-
-public class Student {
-    public String name;
-    public int age;
-    public char gender;
-}
