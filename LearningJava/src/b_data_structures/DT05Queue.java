b4eb9675962351efce5d4bb71e5729954be8fa11
geloodev <angeloholandini11@gmail.com>
2023-08-14 23:32:38 -0300

    finishing queue

--- LearningJava/src/b_data_structures/DT05Queue.java
+++ LearningJava/src/b_data_structures/DT05Queue.java
@@ -1,5 +1,8 @@
 package b_data_structures;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DT05Queue {
 
     public static void main(String[] args) {
@@ -7,5 +10,21 @@ public class DT05Queue {
         // QUEUE:
         // - FIFO (First In First Out)
         // - A collection designed for holding elements prior to processing.
+
+        Queue<Person> supermarket = new LinkedList<>();
+
+        supermarket.add(new Person("Ângelo", 20));
+        supermarket.add(new Person("Filipe", 17));
+        supermarket.add(new Person("Tatiana", 43));
+
+        // peek() retrieve the head of the queue
+        System.out.println("Front item: " + supermarket.peek());
+        System.out.println("Queue size: " + supermarket.size());
+        // poll() retrieve and remove the head of the queue
+        System.out.println("Front item (removed): " + supermarket.poll());
+        System.out.println("Front item after poll(): " + supermarket.peek());
+        System.out.println("Queue size after poll(): " + supermarket.size());
     }
+
+    static record Person(String name, int age) {}
 }
